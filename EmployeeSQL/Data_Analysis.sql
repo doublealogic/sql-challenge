8f4d34cb702d39f42f988636c6a490b635f5cb41
doublealogic <cras.ajrobertson@gmail.com>
2020-11-13 00:30:42 -0500

    Updated Data Analysis file with tasks 2 and 3

--- EmployeeSQL/Data_Analysis.sql
+++ EmployeeSQL/Data_Analysis.sql
@@ -8,3 +8,22 @@ FROM public."Employees"
 INNER JOIN public."Salaries" ON
 public."Employees".emp_no = public."Salaries".emp_no;
 
+-- Lists first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM public."Employees"
+WHERE CAST(hire_date AS text) LIKE '1986%';
+
+-- Lists each department manager's department number, department name, the manager's employee number, last name, first name
+SELECT public."Dept_Manager".dept_no,
+	public."Departments".dept_name,
+	public."Dept_Manager".emp_no,
+	public."Employees".last_name,
+	public."Employees".first_name
+FROM public."Dept_Manager"
+INNER JOIN public."Departments" ON
+public."Dept_Manager".dept_no = public."Departments".dept_no
+INNER JOIN public."Employees" ON
+public."Dept_Manager".emp_no = public."Employees".emp_no;
+
+
+	
