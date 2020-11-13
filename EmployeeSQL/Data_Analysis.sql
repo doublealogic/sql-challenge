8f010866f6fae5bc48e2893b48826bb72e63806e
doublealogic <cras.ajrobertson@gmail.com>
2020-11-13 00:56:21 -0500

    Updated Data Analytics File, added tasks 4 through 7

--- EmployeeSQL/Data_Analysis.sql
+++ EmployeeSQL/Data_Analysis.sql
@@ -25,5 +25,56 @@ public."Dept_Manager".dept_no = public."Departments".dept_no
 INNER JOIN public."Employees" ON
 public."Dept_Manager".emp_no = public."Employees".emp_no;
 
+-- Lists each employee's employee number, last name, first name, and department name.
+SELECT public."Employees".emp_no,
+	public."Employees".last_name,
+	public."Employees".first_name,
+	public."Departments".dept_name
+FROM public."Dept_Manager"
+INNER JOIN public."Departments" ON
+public."Dept_Manager".dept_no = public."Departments".dept_no
+INNER JOIN public."Employees" ON
+public."Dept_Manager".emp_no = public."Employees".emp_no;
 
-	
+-- Lists first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM public."Employees"
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- Lists all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT public."Employees".emp_no,
+	public."Employees".last_name,
+	public."Employees".first_name,
+	public."Departments".dept_name
+FROM public."Dept_Emp"
+INNER JOIN public."Departments" ON
+public."Dept_Emp".dept_no = public."Departments".dept_no 
+INNER JOIN public."Employees" ON
+public."Dept_Emp".emp_no = public."Employees".emp_no
+WHERE public."Dept_Emp".dept_no = 'd007'
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name
+SELECT public."Employees".emp_no,
+	public."Employees".last_name,
+	public."Employees".first_name,
+	public."Departments".dept_name
+FROM public."Dept_Emp"
+INNER JOIN public."Departments" ON
+public."Dept_Emp".dept_no = public."Departments".dept_no
+INNER JOIN public."Employees" ON
+public."Dept_Emp".emp_no = public."Employees".emp_no
+WHERE public."Dept_Emp".dept_no = 'd007'
+
+
+UNION
+
+SELECT public."Employees".emp_no,
+	public."Employees".last_name,
+	public."Employees".first_name,
+	public."Departments".dept_name
+FROM public."Dept_Emp"
+INNER JOIN public."Departments" ON
+public."Dept_Emp".dept_no = public."Departments".dept_no
+INNER JOIN public."Employees" ON
+public."Dept_Emp".emp_no = public."Employees".emp_no
+WHERE public."Dept_Emp".dept_no = 'd005'
